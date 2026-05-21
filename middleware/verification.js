1946673be29399f8b8fe2b7c73d7560a12b58799
manjeshmishragyanu <gyanumishra1225@gmail.com>
2026-05-21 18:44:59 +0530

    fix: verification middleware and null token

--- middleware/verification.js
+++ middleware/verification.js
@@ -1,22 +1,28 @@
 const jwt = require('jsonwebtoken');
 const {JWT_SECRET} = require('../configjwt');
 
-
 function verification(req, res, next){
     const token = req.headers.authorization;
+    
+    if(!token){
+        return res.status(403).json({
+            msg: "No token provided"
+        })
+    }
+    
     const words = token.split(" ");
     const jwtToken = words[1];
 
-    const decodedValue = jwt.verify(jwtToken, JWT_SECRET);
-
-    if(decodedValue.name){
-        next()
-    }else{
-        res.status(403).json({
-            msg: "you are not authenticated"
-        })
+    try {
+        const decodedValue = jwt.verify(jwtToken, JWT_SECRET);
+        if(decodedValue.name){
+            next()
+        } else {
+            res.status(403).json({ msg: "you are not authenticated" })
+        }
+    } catch(err) {
+        res.status(403).json({ msg: "Invalid token" })
     }
-
 }
 
 module.exports = verification;
